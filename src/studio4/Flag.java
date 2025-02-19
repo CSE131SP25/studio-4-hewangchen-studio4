c0a240ebf91f4c7d663de5f0961076d506e3d16e
jonahchen <jonahchen@localhost>
2025-02-19 14:04:45 -0600

    h

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,13 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setPenColor();
+		StdDraw.rectangle(.5, .5,.4 , .4);
+		StdDraw.setPenColor(StdDraw.RED);
+		StdDraw.filledRectangle(.5, .3, .4, .2);
+		StdDraw.setPenColor(StdDraw.BLUE);
+		StdDraw.filledRectangle(.5, .7, .4, .2);
+		StdDraw.setPenColor(StdDraw.YELLOW);
+		StdDraw.filledEllipse(.5, .5, .15, .15);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,49 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shape = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double pOne = in.nextDouble();
+		double pTwo = in.nextDouble();
+		double pThree = in.nextDouble();
+		double pFour = in.nextDouble();
 		
+		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+		if (shape.equals("rectangle")) {
+		if (isFilled == true) {
+			StdDraw.filledRectangle(pOne, pTwo, pThree, pFour);
+		}
+		else {
+			StdDraw.rectangle(pOne, pTwo, pThree, pFour);
+		}
+		StdDraw.show();}
+		else if(shape.equals("ellipse")) {
+			if (isFilled == true) {
+				StdDraw.filledEllipse(pOne, pTwo, pThree, pFour);
+			}
+			else {
+				StdDraw.ellipse(pOne, pTwo, pThree, pFour);
+			}
+			StdDraw.show();
+		}
+		else if(shape.equals("triangle") ) {
+			double pFive = in.nextDouble();
+			double pSix = in.nextDouble();
+			double[] xCoord = {pOne, pThree, pFive};
+			double[] yCoord = {pTwo, pFour, pSix};
+			
+			
+			if (isFilled == true) {
+				StdDraw.filledPolygon(xCoord, yCoord);
+			}
+			else {
+				StdDraw.polygon(xCoord, yCoord);
+			}
+			StdDraw.show();
+			}
+		}
 	}
-}
+
